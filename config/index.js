94ab0989f500d69f0b9ec26c1e525d99f8538208
CuiZhengyang <a2963348@sina.com>
2018-03-26 14:42:41 +0800

    结束订单系统的编码

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/facepayWeixin/',
+    assetsPublicPath: '/plateOrderSystem/client/',
 
     /**
      * Source Maps
